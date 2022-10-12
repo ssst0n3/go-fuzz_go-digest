49ece28d32a4e8b465717565007d44a80f740145
Lei Wang <ssst0n3@gmail.com>
2022-10-12 18:11:15 +0800

    add dockerfiles for go-fuzz-ct

---
+++ .go-fuzz-ct/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+fuzztime=30s
+pkgs=$(git grep 'func Fuzz.*testing\.F' | grep -o '.*\/' | sort | uniq)
+
+for pkg in $pkgs
+do
+    go test -fuzz=. ./$pkg -fuzztime=$fuzztime
+done
